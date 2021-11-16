994e5c366beab26362062a48cdfd464a1e4891e1
Raghava369 <84308405+Raghava369@users.noreply.github.com>
2021-11-16 12:06:38 +0530

    Add files via upload

---
+++ lab program3.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class data
+{
+	private:
+		int feet,inches;
+	public:
+		void set_data()
+         {
+         	cout<<"enter feet and inches"<<endl;
+         	cin>>feet>>inches;
+		 }
+		 void get_data()
+		 {
+			cout<<"Distance is feet= "<<feet<<" "<<"inches = "<<inches<<endl;
+		 }
+		 void add_(data d1,data d2)
+		 {
+		 	feet=d1.feet +d2.feet;
+		 	inches=d1.inches + d2.inches;
+		 	if(inches>=12)
+		 	{
+		 		feet=feet+1;
+		 		inches=inches-12;
+			 }
+		 }
+		void add_(data *d1,data *d2)
+		 {
+		 	feet=d1->feet + d2->feet;
+		 	inches=d1->inches + d2->inches;
+		 	if(inches>=12)
+		 	{
+		 		feet=feet+1;
+		 		inches=inches-12;
+			 }
+		 }
+};
+main()
+{
+	data d1,d2,d3;
+	d1.set_data();
+	d2.set_data();
+	d3.add_(d1,d2); 
+	d3.get_data();
+	d3.add_(&d1,&d2);
+	d3.get_data();
+}
